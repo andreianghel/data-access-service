d0fb406ff9a9f6c0513d74b8278b59d429dfb16c
Andrei Anghel <Andrei.Anghel@belden.com>
2019-02-19 13:57:42 +0200

    initial commit for data-access-service

---
+++ src/main/java/ro/andreianghel/dataaccessservice/DataAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package ro.andreianghel.dataaccessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAccessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAccessServiceApplication.class, args);
+    }
+
+}
